959dcab0f3f9be5019aca73e2a18ede5427c5f83
David Fierstein <studio@davidiad.com>
2017-09-05 22:32:15 -0700

    Fix waypoint camera movement, add video to display 2
    Shader techniques from Marvelous techniques asset in display 2.

---
+++ Assets/scripts/TouchDown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDown : MonoBehaviour {
+
+	public GameObject camContainer;
+	public GameObject go;
+
+	void Start () {
+		Debug.Log ("Start in TouchDown");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log(">>>>>>>>>>>>>>>>");
+			Debug.Log ("Input.GetMouseButtonDown (0)");
+			Debug.Log ("cam pos: " + camContainer.transform.position);
+			Debug.Log ("go pos: " + go.transform.position);
+			Debug.Log ("__________");
+			//			_state = _state == State.Focused ? State.Clicked : _state;
+
+//			_audio_source.Play ();
+
+			camContainer.transform.position = new Vector3(go.transform.position.x, go.transform.position.y, go.transform.position.z);
+			//camCon.transform.SetPositionAndRotation (go.transform.position, Quaternion.identity);
+			//go.transform.position = new Vector3 (go.transform.position.x + 0.01f, go.transform.position.y, go.transform.position.z);
+		}
+	}
+}
